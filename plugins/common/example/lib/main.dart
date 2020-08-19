0909ed8111f67519e19bdb26ca4cbf157a651f0a
xihuiliu <liuxihui168168@163.com>
2020-08-20 01:07:30 +0800

    公共插件

--- plugins/common/example/lib/main.dart
+++ plugins/common/example/lib/main.dart
@@ -27,7 +27,7 @@ class _MyAppState extends State<MyApp> {
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      platformVersion = await Common.platformVersion;
+      platformVersion = await Common.channelName;
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
