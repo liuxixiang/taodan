10576a83c678740f084286015ceab08e249eec4e
liujiawen_kd <wendjia@qq.com>
2020-08-27 16:30:17 +0800

    图片替换脚本修改

--- lib/utils/assets_util.dart
+++ lib/utils/assets_util.dart
@@ -1,4 +1,5 @@
 class AssetsUtil {
   static const String IMAGE_PATH = "assets/images/";
   static const String index_path = IMAGE_PATH + "index/";
+  static const String common_path = IMAGE_PATH + "common/";
 }
