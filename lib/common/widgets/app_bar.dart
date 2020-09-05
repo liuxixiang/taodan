6ad5ba2fb9ebdb2c09daf7a950aca3fce20a7d85
eros <liuxihui168168@163.com>
2020-09-05 16:22:41 +0800

    appbar

---
+++ lib/common/widgets/app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:taodan/utils/assets_util.dart';
+
+AppBar appBar(context,
+    {String title,
+    bool centerTitle = true,
+    Color backgroundColor,
+    Widget rightWidget,
+    VoidCallback onPressed}) {
+  return AppBar(
+    backgroundColor: backgroundColor,
+    leading: IconButton(
+      icon: Image.asset(AssetsUtil.common.ic_wing),
+      onPressed: () {
+        if (onPressed != null) {
+          Navigator.pop(context);
+        }
+      },
+    ),
+    centerTitle: centerTitle,
+    title: Text(title),
+    actions: <Widget>[rightWidget ?? Center()],
+  );
+}
--- lib/page/invite/invite_page.dart
+++ lib/page/invite/invite_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:stacked/stacked.dart';
 import 'package:taodan/common/values/colors.dart';
 import 'package:taodan/common/values/styles.dart';
+import 'package:taodan/common/widgets/app_bar.dart';
 import 'package:taodan/page/invite/invite_viewmodel.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -21,33 +22,31 @@ class _InvitePageState extends State<InvitePage> {
 //         model.request()
       },
       builder: (context, model, child) => Scaffold(
-        body: Container(
-          color: Colors.yellow,
-          child: TextField(
-            // maxLength: 8,
-            //不使用maxLength 右下角会出现1/8
-            inputFormatters: [LengthLimitingTextInputFormatter(8)],
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              //去掉输入框的下滑线
-              fillColor: Colors.white,
-              contentPadding:
-              EdgeInsets.symmetric(vertical: 10.h, horizontal: 20.w),
-              filled: true,
-              enabledBorder: OutlineInputBorder(
-                  borderSide:
-                  BorderSide(color: AppColors.yellow, width: 3.w),
-                  borderRadius: BorderRadius.all(Radius.circular(23))),
-              hintText: '邀请码',
-              hintStyle: AppStyles.textSize15_yellow_black,
-              focusedBorder: OutlineInputBorder(
-                  borderSide:
-                  BorderSide(color: AppColors.yellow, width: 3.w),
-                  borderRadius: BorderRadius.all(Radius.circular(23))),
+          appBar: appBar(context,title: "hahah"),
+          body: Container(
+            color: Colors.yellow,
+            child: TextField(
+              // maxLength: 8,
+              //不使用maxLength 右下角会出现1/8
+              inputFormatters: [LengthLimitingTextInputFormatter(8)],
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                //去掉输入框的下滑线
+                fillColor: Colors.white,
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10.h, horizontal: 20.w),
+                filled: true,
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: AppColors.yellow, width: 3.w),
+                    borderRadius: BorderRadius.all(Radius.circular(23))),
+                hintText: '邀请码',
+                hintStyle: AppStyles.textSize15_yellow_black,
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: AppColors.yellow, width: 3.w),
+                    borderRadius: BorderRadius.all(Radius.circular(23))),
+              ),
             ),
-          ),
-        )
-      ),
+          )),
     );
   }
 }
